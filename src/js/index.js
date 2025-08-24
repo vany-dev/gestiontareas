d97cfcb50b0fb3512df7aac273f62b4eb6b7a2e5
Jhovan <perjhoal@gmail.com>
2025-08-24 11:03:27 -0600

    Actualizando agregar y eliminar tarea

---
+++ src/js/index.js
@@ -0,0 +1,40 @@
+const inputTarea = document.querySelector('#nueva-tarea');
+const selectPrioridad = document.querySelector('#prioridad');
+const btnAgregar = document.querySelector('#btn-agregar');
+const listaTareas = document.querySelector('#tareas-tarjetas');
+
+btnAgregar.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  const tarea = inputTarea.value.trim();
+  const prioridad = selectPrioridad.value;
+
+  if (tarea === '') {
+    alert('Por favor, ingresa una tarea.');
+    return;
+  }
+  if (prioridad === selectPrioridad.options[0].value) {
+    alert('Por favor, selecciona una prioridad.');
+    return;
+  }
+
+  const tarjeta = document.createElement('div');
+  tarjeta.classList.add('tarea', prioridad);
+
+  const textoTarea = document.createElement('p');
+  textoTarea.textContent = tarea;
+
+  const btnEliminar = document.createElement('button');
+  btnEliminar.textContent = 'Eliminar';
+  btnEliminar.addEventListener('click', function() {
+    console.log('Eliminar tarea:', tarea);
+    listaTareas.removeChild(tarjeta);
+  });
+
+  tarjeta.appendChild(textoTarea);
+  tarjeta.appendChild(btnEliminar);
+  listaTareas.appendChild(tarjeta);
+
+  inputTarea.value = '';
+  selectPrioridad.value = 'media';
+});
